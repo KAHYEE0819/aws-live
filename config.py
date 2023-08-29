232efd142345bba2d7978762601ce4f32a065c13
KAHYEE0819 <130823718+KAHYEE0819@users.noreply.github.com>
2023-08-29 16:33:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.ch33mkzylebb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tankahyee-employee"
+customregion = "us-east-1"
+
